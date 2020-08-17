de5d6455379b7f4fcc154649ead50ad660847363
Daniel Rammer <hamersaw@protonmail.com>
2020-08-17 12:44:10 -0600

    computing geohashes for spatial bounding box.

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 ## OVERVIEW
 Spatiotemporal image reconstruction application.
 
+## USAGE
+    ./target/debug/stitch 36.4 36.5 -- -119.7 -119.6 test.tif
+
 ## TODO
 - everything
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use structopt::StructOpt;
+use st_image::prelude::Geocode;
 
 use std::net::IpAddr;
 use std::path::PathBuf;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "example", about = "An example of StructOpt usage.")]
+#[structopt(name="stitch")]
 struct Opt {
     #[structopt(short, long,
         help="stip node ip address", default_value="127.0.0.1")]
@@ -33,6 +34,29 @@ struct Opt {
 }
 
 fn main() {
+    // parse command line options
     let opt = Opt::from_args();
-    println!("{:?}", opt);
+
+    // determine geohashes in bounding box
+    let geocode = Geocode::Geohash;
+    let (longitude_interval, latitude_interval) =
+        geocode.get_intervals(5);
+    let windows = st_image::prelude::get_window_bounds(
+        opt.min_longitude, opt.max_longitude,
+        opt.min_latitude, opt.max_latitude,
+        longitude_interval, latitude_interval);
+
+    // TODO - tmp
+    for (min_long, max_long, min_lat, max_lat) in windows.iter() {
+        let geohash = geocode.get_code((min_long + max_long) / 2.0,
+            (min_lat + max_lat) / 2.0, 5);
+
+        println!("{:?}", geohash);
+    }
+
+    // TODO - locate available geohash images
+
+    // TODO - stitch together image
+    //  - download images
+    //  - generate missing images
 }
