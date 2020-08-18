ed7188fadf71af0dcc37bf43b46f356e1136be1f
Daniel Rammer <hamersaw@protonmail.com>
2020-08-18 15:05:06 -0600

    identifying useful Sentinel-2 images.

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Spatiotemporal image reconstruction application.
 
 ## USAGE
     # retrieve image with latitude bounds [36.4, 36.5],
-    #  longitude bounds [-119.7, -119.6], and timestamp 0
-    ./target/debug/stitch 36.4 36.5 -- -119.7 -119.6 0 test.tif
+    #  longitude bounds [-119.7, -119.6], and date 2018-08-21
+    ./target/debug/stitch 36.4 36.5 -- -119.7 -119.6 1534809600 test.tif
 
 ## TODO
 - everything
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-use protobuf::{ImageBroadcastRequest, ImageBroadcastType, ImageListRequest, ImageManagementClient, Node, NodeLocateRequest, NodeManagementClient};
+use protobuf::{Filter, Image, ImageListRequest, ImageManagementClient, Node, NodeLocateRequest, NodeManagementClient};
 use structopt::StructOpt;
 use st_image::prelude::Geocode;
 use tonic::Request;
 
 use std::error::Error;
-use std::collections::HashMap;
 use std::net::IpAddr;
 use std::path::PathBuf;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name="stitch")]
 struct Opt {
+    #[structopt(short, long, help="stip album", default_value="test")]
+    album: String,
+
     #[structopt(short, long,
         help="stip node ip address", default_value="127.0.0.1")]
     ip_address: IpAddr,
@@ -34,7 +36,7 @@ struct Opt {
     port: u16,
 
     #[structopt(name="TIMESTAMP", help="image timestamp")]
-    timestamp: u64,
+    timestamp: i64,
 
     #[structopt(name="OUTPUT_FILE", help="image output file")]
     output_file: PathBuf,
@@ -63,7 +65,7 @@ fn main() {
             Err(e) => panic!("failed to compute geohash: {}", e),
         };
 
-        println!("processing '{}'", geohash);
+        println!("[+] processing '{}'", geohash);
 
         // find node responsible for this geohash
         let node = match locate_node(&opt.ip_address,
@@ -72,57 +74,27 @@ fn main() {
             Err(e) => panic!("failed to locate node: {}", e),
         };
 
-        println!("  [+] found on node {}", node.id);
+        println!("  [+] found geohash on node {}", node.id);
 
-        // TODO - check for Sentinel-2 image
+        // check for Sentinel-2 image
+        let image = match get_sentinel_image(node.rpc_addr,
+                &opt.album, &geohash, opt.timestamp) {
+            Ok(image) => image,
+            Err(e) => panic!("failed to check Sentinel-2 image: {}", e),
+        };
+
+        if let Some(_) = image {
+            // TODO - add to processing list
+            println!("  [+] found Sentinel-2 image");
+            continue;
+        } else {
+            println!("  [|] unable to find Sentinel-2 image");
+        }
 
         // TODO - check for preceeding Sentinel-2 and MODIS images
     }
 
-    // TODO - locate available geohash images
-    /*for node in node_list_reply.nodes.iter() {
-        // initialize ImageManagement grpc client
-        let mut client = ImageManagementClient::connect(
-            format!("http://{}", node.rpc_addr)).await?;
-
-        // iterate over image stream
-        let mut stream = client.list(Request::new(request.clone()))
-            .await?.into_inner();
-        while let Some(image) = stream.message().await? {
-            for file in image.files.iter() {
-                println!("{:<8}{:<12}{:<10}{:<8}{:<12}{:<16.5}{:<16.5}{:<12}{:<80}",
-                    node.id, image.platform, image.geocode,
-                    image.source, image.timestamp, file.pixel_coverage,
-                    image.cloud_coverage.unwrap_or(-1.0),
-                    file.subdataset, file.path);
-            }
-        }
-    }*/
-
-    /*// initialize Filter
-    let filter = Filter {
-        end_timestamp: opt.timestamp,
-        geocode: geohash,
-        max_cloud_coverage: crate::f64_opt(
-            list_matches.value_of("max_cloud_coverage"))?,
-        min_pixel_coverage: crate::f64_opt(
-            list_matches.value_of("min_pixel_coverage"))?,
-        platform: crate::string_opt(list_matches.value_of("platform")),
-        recurse: list_matches.is_present("recurse"),
-        source: crate::string_opt(list_matches.value_of("source")),
-        start_timestamp: crate::i64_opt(
-            list_matches.value_of("start_timestamp"))?,
-    };
-
-    // initialize ImageListRequest
-    let request = ImageListRequest {
-        album: list_matches.value_of("ALBUM").unwrap().to_string(),
-        filter: filter,
-    };*/
-
-    // TODO - stitch together image
-    //  - download images
-    //  - generate missing images
+    // TODO - process images
 }
 
 #[tokio::main]
@@ -149,31 +121,40 @@ async fn locate_node(ip_address: &IpAddr, port: u16,
     }
 }
 
-/*    // retrieve stip cluster nodes
-    let nodes = match get_nodes(&opt.ip_address, opt.port) {
-        Ok(nodes) => nodes,
-        Err(e) => panic!("failed to retrieve nodes: {}", e),
+#[tokio::main]
+async fn get_sentinel_image(rpc_address: String,
+        album: &str, geohash: &str, timestamp: i64)
+        -> Result<Option<Image>, Box<dyn Error>> {
+    // initialize ImageManagement grpc client
+    let mut client = ImageManagementClient::connect(
+        format!("http://{}", rpc_address)).await?;
+
+    // initialize Filter
+    let filter = Filter {
+        end_timestamp: Some(timestamp + 86400),
+        geocode: Some(geohash.to_string()),
+        max_cloud_coverage: None,
+        min_pixel_coverage: Some(0.95),
+        platform: Some("Sentinel-2".to_string()),
+        recurse: false,
+        source: None,
+        start_timestamp: Some(timestamp - 86400),
     };
 
-#[tokio::main]
-async fn get_nodes(ip_address: &IpAddr, port: u16)
-        -> Result<HashMap<u32, String>, Box<dyn Error>> {
-    // initialize NodeManagement grpc client
-    let mut client = NodeManagementClient::connect(
-        format!("http://{}:{}", ip_address, port)).await?;
+    // initialize ImageListRequest
+    let request = ImageListRequest {
+        album: album.to_string(),
+        filter: filter,
+    };
 
-    // initialize NodeListRequest
-    let node_list_request = Request::new(NodeListRequest {});
+    // iterate over image stream
+    let mut stream = client.list(Request::new(request.clone()))
+        .await?.into_inner();
 
-    // retrieve NodeListReply
-    let node_list_reply = client.list(node_list_request).await?;
-    let node_list_reply = node_list_reply.get_ref();
-
-    // populate nodes map
-    let mut nodes = HashMap::new();
-    for node in node_list_reply.nodes.iter() {
-        nodes.insert(node.id, node.xfer_addr.clone());
+    let mut image = None;
+    while let Some(image_proto) = stream.message().await? {
+        image = Some(image_proto);
     }
 
-    Ok(nodes)
-}*/
+    Ok(image)
+}
