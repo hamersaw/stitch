184bf91fca4857a0b3a7ff094e511794a2fe46b1
Daniel Rammer <hamersaw@protonmail.com>
2020-10-05 13:24:50 -0600

    added cluster start and stop scripts.

---
+++ sbin/start-all.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# check arguments
+if [ $# != 0 ]; then
+    echo "usage: $(basename $0)"
+    exit
+fi
+
+# compute project directory and hostfile locations
+projectdir="$(pwd)/$(dirname $0)/.."
+hostfile="$projectdir/etc/hosts.txt"
+
+# initialize instance variables
+if [ -f "$projectdir/impl/stipd/target/debug/stitchd" ]; then
+    application="$projectdir/impl/stipd/target/debug/stitchd"
+fi
+
+if [ -f "$projectdir/impl/target/debug/stitchd" ]; then
+    application="$projectdir/impl/target/debug/stitchd"
+fi
+
+if [ -z "$application" ]; then
+    echo "'stitchd' binary not found."
+    exit
+fi
+
+# iterate over hosts
+nodeid=0
+while read line; do
+    # parse host, port, and options
+    host=$(echo $line | awk '{print $1}')
+    rpcport=$(echo $line | awk '{print $2}')
+    options=$(echo $line | cut -d' ' -f3-)
+
+    echo "starting node $nodeid"
+    if [ $host == "127.0.0.1" ]; then
+        # start application locally
+        RUST_LOG=debug $application -i $host -p $rppport $options \
+            > $projectdir/log/node-$nodeid.log 2>&1 &
+
+        echo $! > $projectdir/log/node-$nodeid.pid
+    else
+        # start application on remote host
+        ssh rammerd@$host -n "RUST_LOG=debug \
+            $application -i $host -p $rpcport $options \
+                > $projectdir/log/node-$nodeid.log 2>&1 & \
+            echo \$! > $projectdir/log/node-$nodeid.pid"
+    fi
+
+    # increment node id
+    (( nodeid += 1 ))
+done <$hostfile
